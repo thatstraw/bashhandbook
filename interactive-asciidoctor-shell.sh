c363cbdd6d639ed69364774febedf984af6376d7
Traw <valentine.nachi@iu-study.org>
2025-06-23 23:39:41 +0400

    fix: correct regex tables and Docker scripts
    Fix regular expressions tables and Docker scripts
    
    Fix table parsing issues in chapter-19-Regular-Expressions and update Docker scripts

--- interactive-asciidoctor-shell.sh
+++ interactive-asciidoctor-shell.sh
@@ -2,4 +2,9 @@
 # Docker image: https://hub.docker.com/r/asciidoctor/docker-asciidoctor
 # Docker image repository: https://github.com/asciidoctor/docker-asciidoctor
 
-docker run -it -v $(pwd)/book:/documents/ asciidoctor/docker-asciidoctor
+# Use a custom Docker image with pygments.rb and Python installed
+DOCKER_IMAGE=custom-asciidoctor-pdf:pygments
+
+docker build -t $DOCKER_IMAGE -f Dockerfile.asciidoctor-pdf .
+
+docker run -it -w /documents -v $(pwd)/book:/documents/ $DOCKER_IMAGE sh
